e1263cc3aa8236e274eb08e881b7a92fa72e8342
Michael Thaler <michael.thaler@dlr.de>
2021-06-11 14:21:12 +0200

    Updated RestApiDemoController.

--- src/main/kotlin/com/mthaler/sburrestdemo/RestApiDemoController.kt
+++ src/main/kotlin/com/mthaler/sburrestdemo/RestApiDemoController.kt
@@ -1,36 +1,39 @@
 package com.mthaler.sburrestdemo
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/coffees")
 class RestApiDemoController {
 
     private val coffees = arrayListOf(Coffee("Café Cereza"), Coffee("Café Ganador"), Coffee("Café Lareño"), Coffee("Café Três Pontas"))
 
-    @GetMapping("/coffees")
+    @GetMapping
     fun getCoffees(): Iterable<Coffee> = coffees
 
-    @GetMapping("/coffees/{id}")
+    @GetMapping("/{id}")
     fun getCoffeeById(@PathVariable id: String): Coffee? = coffees.find { it.id == id }
 
-    @PostMapping("/coffees")
+    @PostMapping
     fun postCoffee(@RequestBody coffee: Coffee): Coffee {
         coffees.add(coffee)
         return coffee
     }
 
-    @PutMapping("/coffees/{id}")
-    fun putCoffee(@PathVariable id: String, @RequestBody coffee: Coffee): Coffee {
+    @PutMapping("/{id}")
+    fun putCoffee(@PathVariable id: String, @RequestBody coffee: Coffee): ResponseEntity<Coffee> {
         val index = coffees.indexOfFirst { it.id == id }
         if (index >= 0) {
             coffees.set(index, coffee)
+            return ResponseEntity(coffee, HttpStatus.OK)
         } else {
-            coffees.add(coffee)
+            return ResponseEntity(postCoffee(coffee), HttpStatus.CREATED)
         }
-        return coffee
     }
 
-    @DeleteMapping("/coffees/{id}")
+    @DeleteMapping("/{id}")
     fun deleteCoffee(@PathVariable id: String) {
         coffees.removeIf { it.id == id }
     }
