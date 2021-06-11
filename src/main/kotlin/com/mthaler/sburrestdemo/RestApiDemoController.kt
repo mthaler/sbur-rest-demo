cfa5be7db1b5cf7e42a8814151a3ffdcf42f950b
Michael Thaler <michael.thaler@dlr.de>
2021-06-11 13:15:22 +0200

    Updated RestApiDemoController.

--- src/main/kotlin/com/mthaler/sburrestdemo/RestApiDemoController.kt
+++ src/main/kotlin/com/mthaler/sburrestdemo/RestApiDemoController.kt
@@ -1,13 +1,11 @@
 package com.mthaler.sburrestdemo
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class RestApiDemoController {
 
-    private val coffees = listOf(Coffee("Café Cereza"), Coffee("Café Ganador"), Coffee("Café Lareño"), Coffee("Café Três Pontas"))
+    private val coffees = arrayListOf(Coffee("Café Cereza"), Coffee("Café Ganador"), Coffee("Café Lareño"), Coffee("Café Três Pontas"))
 
     @GetMapping("/coffees")
     fun getCoffees(): Iterable<Coffee> = coffees
@@ -15,4 +13,25 @@ class RestApiDemoController {
     @GetMapping("/coffees/{id}")
     fun getCoffeeById(@PathVariable id: String): Coffee? = coffees.find { it.id == id }
 
+    @PostMapping("/coffees")
+    fun postCoffee(@RequestBody coffee: Coffee): Coffee {
+        coffees.add(coffee)
+        return coffee
+    }
+
+    @PutMapping("/coffees/{id}")
+    fun putCoffee(@PathVariable id: String, @RequestBody coffee: Coffee): Coffee {
+        val index = coffees.indexOfFirst { it.id == id }
+        if (index >= 0) {
+            coffees.set(index, coffee)
+        } else {
+            coffees.add(coffee)
+        }
+        return coffee
+    }
+
+    @DeleteMapping("/coffees/{id}")
+    fun deleteCoffee(@PathVariable id: String) {
+        coffees.removeIf { it.id == id }
+    }
 }
